4406c9a059d1dfe0bbb7de81f8ce9ce204975281
Jabi <michal.jabi@gmail.com>
2025-11-26 12:46:16 +0100

    task(03): make it work: ng g c sample-counter

--- ng-living-theory/src/app/app.component.ts
+++ ng-living-theory/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './menu.component';
+import { SampleCounter } from './sample-counter/sample-counter';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuComponent],
+  imports: [RouterOutlet, MenuComponent, SampleCounter],
   template: `
     <main class="container mb-5">
       <header class="hero is-info">
@@ -13,6 +14,13 @@ import { MenuComponent } from './menu.component';
           <h4 class="subtitle">sprawdź jak działa {{ title }}!...</h4>
         </div>
       </header>
+      HELLO...
+      <!-- TUTAJ osadź komponent -->
+      <app-sample-counterz />
+      <app-sample-counterz />
+      <app-sample-counterz />
+      <app-sample-counterz />
+      <app-sample-counterz />
       <section class="columns mt-5">
         <app-menu></app-menu>
         <div class="column is-9 content">
@@ -21,6 +29,7 @@ import { MenuComponent } from './menu.component';
       </section>
     </main>
   `,
+  styles: [],
 })
 export class AppComponent {
   title = 'ng-living-theory !!';
---
+++ ng-living-theory/src/app/sample-counter/sample-counter.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sample-counterz',
+  imports: [],
+  template: ` <div>sample-counter works!</div> `,
+  styles: ``,
+})
+export class SampleCounter {}
+
+// const sc = new SampleCounter();
