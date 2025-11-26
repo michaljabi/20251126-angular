79cbec09a4ceb8fddfb887ad7eaaedd02967cebe
Jabi <michal.jabi@gmail.com>
2025-11-26 16:04:17 +0100

    task(07): ng g c header

---
+++ auction-portal/src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  template: `
+    <header class="mb-2 p-5 bg-warning">
+      <h1>Auction portal</h1>
+    </header>
+  `,
+  styles: ``,
+})
+export class HeaderComponent {}
