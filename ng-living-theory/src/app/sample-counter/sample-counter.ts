57d0134aa090abfd66abe3386bb7f8af7e1148b4
Jabi <michal.jabi@gmail.com>
2025-11-26 12:57:27 +0100

    explain(03): sample-counter - data binding example.

--- ng-living-theory/src/app/sample-counter/sample-counter.ts
+++ ng-living-theory/src/app/sample-counter/sample-counter.ts
@@ -3,9 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sample-counterz',
   imports: [],
-  template: ` <div>sample-counter works!</div> `,
+  template: ` <div>sample-counter works! {{ myLuckyNumber }}</div> `,
   styles: ``,
 })
-export class SampleCounter {}
+export class SampleCounter {
+  myLuckyNumber = 30 * Math.random();
+
+  constructor() {
+    setTimeout(() => {
+      console.log(this);
+      this.myLuckyNumber = 200;
+    }, 3000);
+  }
+}
 
 // const sc = new SampleCounter();
