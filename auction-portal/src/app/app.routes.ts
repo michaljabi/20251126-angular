d1d22a26b7c4648ae95b16296772996586ff04c1
Jabi <michal.jabi@gmail.com>
2025-11-27 14:18:38 +0100

    explain: redirect + spoiler about 404 (catch all): '**' wildcard

--- auction-portal/src/app/app.routes.ts
+++ auction-portal/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { AuctionsPageComponent } from './auctions/auctions-page.component';
-
+// import { HeaderComponent } from './header/header.component';
 
 export const routes: Routes = [
-    { path: 'aucitons', component: AuctionsPageComponent }
+  { path: '', redirectTo: '/aucitons', pathMatch: 'full' },
+  { path: 'aucitons', component: AuctionsPageComponent },
+  // { path: '**', component: HeaderComponent }
 ];
