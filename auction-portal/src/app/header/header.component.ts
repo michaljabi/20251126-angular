06d7727578b75eba0b6ae6eefb28f1f629ec2d11
Jabi <michal.jabi@gmail.com>
2025-11-27 09:43:29 +0100

    task(08): make it work with `signals` -> v21 standard.

--- auction-portal/src/app/header/header.component.ts
+++ auction-portal/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,13 +6,27 @@ import { Component } from '@angular/core';
   template: `
     <header class="mb-2 p-5 bg-warning">
       <h1>{{ appTitle }}</h1>
-      <h6>{{ subTitle }}</h6>
+      <h6>{{ subTitle() }}</h6>
     </header>
   `,
   styles: ``,
 })
 export class HeaderComponent {
-  appTitle: string = 'Auction Portal!';
+  appTitle = 'Auction Portal!';
 
-  subTitle = "...znajdziesz zawsze to czego szukasz";
+  //protected subTitle = "...znajdziesz zawsze to czego szukasz";
+
+  // to jest STANDARD od ng v21
+  subTitle = signal('...znajdziesz zawsze to czego szukasz');
+
+  constructor() {
+    setTimeout(() => {
+      this.appTitle += '?'; // to zadziała bo mamy zone.js lib -> mechanizm "change detection"
+
+      // Ustawienie wartości sygnału:
+      // this.subTitle.set('?');
+      // Zmiana aktualnej wartości sygnału:
+      this.subTitle.update((v) => v + '?');
+    }, 3000);
+  }
 }
