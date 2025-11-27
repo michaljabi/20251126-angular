0b749920f776074427cbf2e7765d8a275f3495b5
Jabi <michal.jabi@gmail.com>
2025-11-27 15:12:57 +0100

    explain: RWD + Mobie first in bootstrap.

--- auction-portal/src/app/auctions/auctions-page.component.ts
+++ auction-portal/src/app/auctions/auctions-page.component.ts
@@ -3,12 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   imports: [],
   template: `
-    <p>
-      auctions-page works!
-    </p>
+    <section>
+      <h2>Lista naszych aukcji</h2>
+      <div class="row">
+        @for(x of [1,2,3,4,5]; track x) {
+           <div class="col-12 col-sm-6 col-md-4 col-lg-3">[{{ x }}]</div>
+        }
+      </div>
+    </section>
   `,
   styles: ``,
 })
-export class AuctionsPageComponent {
-
-}
+export class AuctionsPageComponent {}
