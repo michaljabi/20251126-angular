35245e2e9838c6f3229ddc1ac1056f9b3f211800
Jabi <michal.jabi@gmail.com>
2025-11-27 11:37:12 +0100

    refactor(11): using signal().

--- auction-portal/src/app/main-menu/main-menu.component.ts
+++ auction-portal/src/app/main-menu/main-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-main-menu',
@@ -9,8 +9,23 @@ import { Component } from '@angular/core';
 export class MainMenuComponent {
   isMenuOpen = false;
 
+  // Sn jest dodane tylko dla tego że powyżej używam już isMenuOpen
+  isMenuOpenSn = signal(false);
+
   toggleMenu() {
     // console.log('!')
     this.isMenuOpen = !this.isMenuOpen;
+    // podejście sygnałowe:
+
+    // this.isMenuOpenSn.set(true)
+
+    // jeśli chcemy odczytać wartość sygnału
+    console.log(this.isMenuOpenSn())
+
+    // takie podejście jest akceptowalne:
+    // this.isMenuOpenSn.set(!this.isMenuOpenSn())
+
+    // ale TO - jest BEZPIECZNIEJSZE:
+    this.isMenuOpenSn.update((v) => !v);
   }
 }
