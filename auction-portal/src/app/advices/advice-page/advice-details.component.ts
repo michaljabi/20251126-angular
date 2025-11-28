c1a5b1bd4cd299d732dee65c0d6b82f917cdc13d
Jabi <michal.jabi@gmail.com>
2025-11-28 13:01:22 +0100

    explain: parasm Observable subscribe - to get info about params in route.

--- auction-portal/src/app/advices/advice-page/advice-details.component.ts
+++ auction-portal/src/app/advices/advice-page/advice-details.component.ts
@@ -1,5 +1,5 @@
 import { JsonPipe } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -8,10 +8,23 @@ import { ActivatedRoute } from '@angular/router';
   template: ` <p>advice-details works! {{ activatedRoute.snapshot.params | json }}</p> `,
   styles: ``,
 })
-export class AdviceDetailsComponent implements OnInit {
+export class AdviceDetailsComponent implements OnInit, OnDestroy {
   activatedRoute = inject(ActivatedRoute);
 
   ngOnInit(): void {
-    console.log('Nowy param:', this.activatedRoute.snapshot.params['adviceId']);
+    // console.log('Nowy param:', this.activatedRoute.snapshot.params['adviceId']);
+    this.activatedRoute.params.subscribe((params) => {
+      console.log('Faktyczny nowy param:', params['adviceId']);
+      this.loadAdvice(params['adviceId']);
+    });
+  }
+
+  ngOnDestroy(): void {
+    console.log('Komponent AdviceDetailsComponent - zniszczony!');
+  }
+
+  loadAdvice(id: string) {
+    // uruchamiamy jakiś service to wyświetlenia treści advice....
+    console.warn('Ładuje informacje dla Advice id', id);
   }
 }
