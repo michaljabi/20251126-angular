9853980adea25eb6ecab7f2dea86eaadd8bf22e3
Jabi <michal.jabi@gmail.com>
2025-11-28 12:35:06 +0100

    task(19): preparation for child routing in advices/

--- auction-portal/src/app/app.routes.ts
+++ auction-portal/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { AuctionsPageComponent } from './auctions/auctions-page.component';
 import { PromotionsPageComponent } from './auctions/promotions-page/promotions-page.component';
 import { AddAuctionPageComponent } from './auctions/add-auction-page/add-auction-page.component';
 import { CartPageComponent } from './auctions/cart-page/cart-page.component';
+import { AdvicePageComponent } from './advices/advice-page/advice-page.component';
 // import { HeaderComponent } from './header/header.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/auctions', pathMatch: 'full' },
   { path: 'auctions', component: AuctionsPageComponent },
   { path: 'promotions', component: PromotionsPageComponent },
+  { path: 'advices', component: AdvicePageComponent },
   { path: 'add-auction', component: AddAuctionPageComponent },
   { path: 'cart', component: CartPageComponent },
   // { path: '**', component: HeaderComponent }
