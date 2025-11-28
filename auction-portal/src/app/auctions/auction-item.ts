eb286f00bb334f858876a3536d575082e2a32aff
Jabi <michal.jabi@gmail.com>
2025-11-28 09:41:39 +0100

    task(16): make it work. Iterface + service.

--- auction-portal/src/app/auctions/auction-item.ts
+++ auction-portal/src/app/auctions/auction-item.ts
@@ -1,3 +1,14 @@
+// type Money = {value: number, currency: 'PLN' | 'USD' | 'EUR'};
+type Money = number;
+
+/*
+    Reprezentacja typu danych który będziemy odbierać z back-endu.
+*/
 export interface AuctionItem {
     // TODO...
+    id: string;
+    title: string;
+    imgUrl: string;
+    price: Money;
+    description?: string;
 }
